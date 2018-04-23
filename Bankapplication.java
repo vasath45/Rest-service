680b0e25d4f07aeaff3566367ac6be443a016a32
vasath45 <37019334+vasath45@users.noreply.github.com>
2018-04-23 17:14:03 +0530

    Update Bankapplication.java

--- Bankapplication.java
+++ Bankapplication.java
@@ -14,7 +14,7 @@ Statement stt =conn.createStatement();
 ResultSet rs=stt.executeQuery(query1);
 while(rs.next())
 {
-System.out.println(rs.getString(1)+"\t"+rs.getString(2)+"\t"+rs.getInt(3)+"\t"+rs.getString(4)+"\t"+rs.getString(5)+"\t"+rs.getString(6)+"\t"+rs.getString(7)+"\t"+rs.getString(80));
+System.out.println(rs.getString(1)+"\t"+rs.getString(2)+"\t"+rs.getInt(3)+"\t"+rs.getString(4)+"\t"+rs.getString(5)+"\t"+rs.getString(6)+"\t"+rs.getString(7)+"\t"+rs.getString(8));
 }
 conn.close();
 }
